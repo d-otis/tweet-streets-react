32069b9ff60aa5aebd62137f79354a3e876b3ea2
d-otis <dan@danfoley.dev>
2022-02-26 13:45:34 -0500

    setup routing to Tweets

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Tweets from "./components/Tweets"
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="tweets" element={<Tweets />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
